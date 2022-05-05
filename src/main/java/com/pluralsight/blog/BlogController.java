20685ad40ac59faea268dc7c21432a3ba13b652b
F5166616 <Robson.Mundere@fnb.co.za>
2022-05-05 09:28:51 +0200

    added mock data.

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,26 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+
+    private final PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
         modelMap.put("title", "Blog Post 1");
-    return "home";
+        List<Post> allPosts = postRepository.getAllPosts();
+        return "home";
 }
 }
